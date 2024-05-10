445d2d02fa5e043bcea78b687680cc251fc380ff
superTng <73048117+superTng@users.noreply.github.com>
2024-05-10 20:59:33 +0800

    Initial commit

---
+++ OpenGLExcercise01/main.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include<GLFW/glfw3.h>
+
+
+int main()
+{
+	printf("ÄãºÃ!");
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+	glfwWindowHint();
+	return 0;
+}
