368956f0c27b856a206708c0837971dc69bde633
superTng <73048117+superTng@users.noreply.github.com>
2024-05-10 21:33:17 +0800

    window001
    完成 hello window1

--- OpenGLExcercise01/main.cpp
+++ OpenGLExcercise01/main.cpp
@@ -7,9 +7,40 @@
 
 int main()
 {
-	printf("你好!");
+	printf("开始绘制窗口!");
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
-	glfwWindowHint();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
+
+	//打开OpenGL glfw window 
+	GLFWwindow* window = glfwCreateWindow(900,600,"Hello World",NULL,NULL);
+	if (window == NULL)
+	{
+		printf("打开Window失败！");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//init glew
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK)
+	{
+		printf("初始化 GLEW 失败！");
+		glfwTerminate();
+		return -1;
+	}
+	glViewport(0,0,900,600);
+
+	//如果这个窗口没有关闭
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
